d378939b090ca752e6c7659702e6cd4d0218ced0
kamyla-vandoski <vandoski.kamyla@escola.pr.gov.br>
2025-06-26 08:20:29 -0300

    fda

--- assets/pergunta.js
+++ assets/pergunta.js
@@ -1,4 +1,16 @@
 CriaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+CriaCartao(
+    'Ciencias',
+    '',
+    respota
+)
+
+CriaCartao(
     categoria,
     pergunta,
     respota
